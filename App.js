b5d40cc9832bbead83e86de74335cde98dcccf8f
sarita <aritasp204@gmail.com>
2022-07-24 13:18:04 +0530

    Gmail is fixed

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Google from './Google'
+
+function App() {
+  return (
+    <div className="App">
+      <Google />
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ Google.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+class Google extends Component {
+    render() {
+        return (
+            <div className="header-component">
+
+                <div className="link-component">
+                    <div className="header-link">Gmail</div>
+                    <div className="subheader-link">Images</div>
+                </div>
+    
+    <div className="menu-icon">
+    <a class="gb_A" aria-label="Google apps" href="https://www.google.co.in/intl/en/about/products" aria-expanded="false" role="button" tabindex="0"><svg class="gb_Ue" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg></a>
+    </div>  
+
+            <div>
+                <div class="sign">Sign in</div>
+            </div>
+         </div>              
+            
+        );
+    }
+}
+
+export default Google;
+
